24eff32b343fb18c6a6c40e937d61aa6be6cf0a4
Tejaskarthik <tejaskarthik2020@gmail.com>
2026-02-27 15:28:59 +0530

    UC1: Add welcome message module

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        // --------------------------------------
+// UC1: Welcome message
+// --------------------------------------
+        System.out.println("======================================");
+        System.out.println("     PALINDROME CHECKER APPLICATION    ");
+        System.out.println("======================================");
+        System.out.println("Version : 1.0");
+        System.out.println("Author  : TEEKAY");
+        System.out.println("--------------------------------------");
+        System.out.println("This application checks if a string is");
+        System.out.println("a palindrome. Let's begin!");
+        System.out.println("--------------------------------------\n");
+    }
+}
