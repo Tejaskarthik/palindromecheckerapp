c86cd8d8dd5ab6b13cc67bfdec24b95a24ff9911
Tejaskarthik <tejaskarthik2020@gmail.com>
2026-02-27 15:32:25 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,18 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        // --------------------------------------
-// UC1: Welcome message
-// --------------------------------------
+
+    public static void main(String[] args) {
+
+        // UC1: Welcome message
         System.out.println("======================================");
         System.out.println("     PALINDROME CHECKER APPLICATION    ");
         System.out.println("======================================");
         System.out.println("Version : 1.0");
-        System.out.println("Author  : TEEKAY");
+        System.out.println("Author  : KOKKI Kumar");
         System.out.println("--------------------------------------");
         System.out.println("This application checks if a string is");
         System.out.println("a palindrome. Let's begin!");
         System.out.println("--------------------------------------\n");
+
+        // --------------------------------------
+        // UC2: Hardcoded palindrome check (PUT HERE)
+        // --------------------------------------
+        String word = "MADAM";
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        System.out.println("UC2: Checking hardcoded word...");
+        System.out.println("Input Word: " + word);
+
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println("Result: " + word + " is a Palindrome\n");
+        } else {
+            System.out.println("Result: " + word + " is NOT a Palindrome\n");
+        }
     }
 }
